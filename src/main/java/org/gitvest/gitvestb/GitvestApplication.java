160def27fc48928e4103ce4845f9a8d973ef9575
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-11-24 20:42:41 +0900

    build: 프로젝트 셋팅
    기본 프로젝트 셋팅
    - java 17
    - springboot 3.4

---
+++ src/main/java/org/gitvest/gitvestb/GitvestApplication.java
@@ -0,0 +1,13 @@
+package org.gitvest.gitvestb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitvestApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GitvestApplication.class, args);
+  }
+
+}
---
+++ src/test/java/org/gitvest/gitvestb/GitvestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gitvest.gitvestb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitvestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
