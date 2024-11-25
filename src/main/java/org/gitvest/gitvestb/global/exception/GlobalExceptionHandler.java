62b7da0d01ba729f04ca02b64a8383907567f79b
sktkddn777 <sktkddn777@naver.com>
2024-11-25 22:27:51 +0900

    build: global 도메인 생성
    - global 도메인 생성

---
+++ src/main/java/org/gitvest/gitvestb/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,11 @@
+package org.gitvest.gitvestb.global.exception;
+
+import lombok.Getter;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Getter
+public class GlobalExceptionHandler {
+
+
+}
---
+++ src/main/java/org/gitvest/gitvestb/global/utils/StringUtil.java
@@ -0,0 +1,11 @@
+package org.gitvest.gitvestb.global.utils;
+
+public class StringUtil {
+
+  /**
+   * 입력받은 문자열이 Null 인지 확인해주는 메서드
+   */
+  public static boolean isNull(String input) {
+    return input == null;
+  }
+}
