49ebbdba5ae36acfc9ba9f0301e126aaa1e9f649
kth0706 <pooiiiu4100@gmail.com>
2024-11-26 15:39:10 +0900

    feat: Global Exception Handler 적용

---
+++ src/main/java/org/gitvest/gitvestb/global/exception/CustomException.java
@@ -0,0 +1,14 @@
+package org.gitvest.gitvestb.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException{
+
+  private ExceptionEnum error;
+
+  public CustomException(ExceptionEnum e){
+    super(e.getErrorMessage());
+    this.error = e;
+  }
+}
