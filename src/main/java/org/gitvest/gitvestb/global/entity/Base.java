d8b798e5c48415eb1c920d38ef6c600ce042d1c2
jeonjuho <wjswngh23@daum.net>
2024-11-29 14:04:16 +0900

    feat(Base): Base 엔티티 생성
    각 엔티티 객체의 생성, 수정, 삭제 시간을 관리할 엔티티

---
+++ src/main/java/org/gitvest/gitvestb/global/entity/Base.java
@@ -0,0 +1,42 @@
+package org.gitvest.gitvestb.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import java.time.LocalDateTime;
+import lombok.experimental.SuperBuilder;
+
+@MappedSuperclass
+@SuperBuilder
+public abstract class Base {
+
+  @Column(name = "createdAt", updatable = false, nullable = false)
+  private LocalDateTime createdAt;
+
+  @Column(name = "updatedAt", nullable = false)
+  private LocalDateTime updatedAt;
+
+  @Column(name = "deletedAt")
+  private LocalDateTime deletedAt;
+
+  public Base() {
+  }
+
+  @PrePersist
+  public void prePersist() {
+    LocalDateTime now = now();
+    createdAt = now;
+    updatedAt = now;
+  }
+
+  @PreUpdate
+  public void preUpdate() {
+    updatedAt = now();
+  }
+
+  private LocalDateTime now() {
+    return LocalDateTime.now();
+  }
+
+}
