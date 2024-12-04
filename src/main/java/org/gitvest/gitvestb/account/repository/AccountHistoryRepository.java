938449d4a515cff434337c62905f294b04bfac30
Hyeon-Uk <rlagusdnr120@gmail.com>
2024-12-04 17:34:46 +0900

    fix: AccountHistoryRepository의 제네릭 오타 수정

--- src/main/java/org/gitvest/gitvestb/account/repository/AccountHistoryRepository.java
+++ src/main/java/org/gitvest/gitvestb/account/repository/AccountHistoryRepository.java
@@ -1,7 +1,8 @@
 package org.gitvest.gitvestb.account.repository;
 
+import org.gitvest.gitvestb.account.entity.AccountHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AccountHistoryRepository extends JpaRepository<AccountRepository, Long> {
+public interface AccountHistoryRepository extends JpaRepository<AccountHistory, Long> {
 
 }
