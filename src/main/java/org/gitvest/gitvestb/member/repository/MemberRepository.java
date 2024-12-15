6d14a7da43a6870d074f5f763b4444075495371d
jeonjuho <wjswngh23@daum.net>
2024-12-15 17:35:56 +0900

    refactor(MemberRepository): MemberRepositoryImp 클래스에서 메서드를 대체하여 삭제
    - MemberRepositoryImp 클래스에서 QueryDsl로 동일한 기능의 메서드를 생성하여 삭제

--- src/main/java/org/gitvest/gitvestb/member/repository/MemberRepository.java
+++ src/main/java/org/gitvest/gitvestb/member/repository/MemberRepository.java
@@ -1,19 +1,15 @@
 package org.gitvest.gitvestb.member.repository;
 
-import java.util.Optional;
 import org.gitvest.gitvestb.member.entity.Member;
-import org.gitvest.gitvestb.member.repository.dto.MemberProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-public interface MemberRepository extends JpaRepository<Member, Long> {
+public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom{
 
-  @Query(
-      "SELECT new org.gitvest.gitvestb.member.repository.dto.MemberProfile(m.profileImageUrl, m.nickname, a.balance)"
-          + " FROM Member m"
-          + " JOIN Account a ON m.memberId = a.member.memberId"
-          + " WHERE m.memberId = :memberId")
-  Optional<MemberProfile> findMemberProfile(@Param("memberId") Long memberId);
+//  @Query(
+//      "SELECT new org.gitvest.gitvestb.member.repository.dto.MemberProfile(m.profileImageUrl, m.nickname, a.balance)"
+//          + " FROM Member m"
+//          + " JOIN Account a ON m.memberId = a.member.memberId"
+//          + " WHERE m.memberId = :memberId")
+//  Optional<MemberProfile> findMemberProfile(@Param("memberId") Long memberId);
 
 }
