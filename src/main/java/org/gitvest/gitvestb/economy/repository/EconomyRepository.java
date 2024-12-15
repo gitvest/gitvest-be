955ed5e135a885fa211fb20106214686513a818a
kth0706 <pooiiiu4100@gmail.com>
2024-12-15 23:55:20 +0900

    feat: Mission 엔티티 레포지토리 추가

---
+++ src/main/java/org/gitvest/gitvestb/economy/repository/EconomyRepository.java
@@ -0,0 +1,11 @@
+package org.gitvest.gitvestb.economy.repository;
+
+import org.gitvest.gitvestb.economy.entity.Mission;
+import org.gitvest.gitvestb.economy.repository.dto.MissionAttendance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EconomyRepository extends JpaRepository<Mission, Long>, EconomyRepositoryCustom {
+  MissionAttendance getLastAccessDate(Long memberId);
+}
