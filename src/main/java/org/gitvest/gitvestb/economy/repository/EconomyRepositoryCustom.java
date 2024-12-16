27e6e6ca33987d2b400e4877a2b06876427eaa8b
kth0706 <pooiiiu4100@gmail.com>
2024-12-16 14:35:07 +0900

    feat: Mission 엔티티 출석 일자 반영을 위한 Update 쿼리 작성
    - setLastAccessDate
    - MemberId에 해당하는 데이터의 LastAccessDate를 현재 날짜로 변경

--- src/main/java/org/gitvest/gitvestb/economy/repository/EconomyRepositoryCustom.java
+++ src/main/java/org/gitvest/gitvestb/economy/repository/EconomyRepositoryCustom.java
@@ -1,7 +1,9 @@
 package org.gitvest.gitvestb.economy.repository;
 
+import java.time.LocalDate;
 import org.gitvest.gitvestb.economy.repository.dto.MissionAttendance;
 
 public interface EconomyRepositoryCustom {
   MissionAttendance getLastAccessDate(Long memberId);
+  long setLastAccessDate(LocalDate cur, Long memberId);
 }
--- src/main/java/org/gitvest/gitvestb/economy/repository/EconomyRepositoryImpl.java
+++ src/main/java/org/gitvest/gitvestb/economy/repository/EconomyRepositoryImpl.java
@@ -3,9 +3,11 @@ package org.gitvest.gitvestb.economy.repository;
 
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.gitvest.gitvestb.economy.entity.QMission;
 import org.gitvest.gitvestb.economy.repository.dto.MissionAttendance;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 public class EconomyRepositoryImpl implements EconomyRepositoryCustom{
@@ -24,5 +26,16 @@ public class EconomyRepositoryImpl implements EconomyRepositoryCustom{
         .from(mission)
         .where(mission.member.memberId.eq(memberId))
         .fetchOne();
+  }
+
+  @Override
+  @Transactional
+  public long setLastAccessDate(LocalDate cur, Long memberId) {
+    QMission mission = QMission.mission;
+    return queryFactory
+        .update(mission)
+        .set(mission.lastAccessDate, cur)
+        .where(mission.member.memberId.eq(memberId))
+        .execute();
   }
 }
