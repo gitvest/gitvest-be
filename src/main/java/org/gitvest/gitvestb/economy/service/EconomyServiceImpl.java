d3f0224331737337e332bb53088ed1cddc867a8a
kth0706 <pooiiiu4100@gmail.com>
2024-12-16 14:43:07 +0900

    feat: 출석 체크 비즈니스 로직 작성
    - 현재 날짜와 비교
    - 현재 날짜와 같지 않을 경우 현재 날짜 Update

---
+++ src/main/java/org/gitvest/gitvestb/economy/service/EconomyServiceImpl.java
@@ -0,0 +1,26 @@
+package org.gitvest.gitvestb.economy.service;
+
+import java.time.LocalDate;
+import lombok.RequiredArgsConstructor;
+import org.gitvest.gitvestb.economy.repository.EconomyRepository;
+import org.gitvest.gitvestb.economy.repository.dto.MissionAttendance;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EconomyServiceImpl implements EconomyService {
+
+  private final EconomyRepository economyRepository;
+
+  @Override
+  public void checkAttendance(Long memberId) {
+    // TODO : 에러 처리
+
+    LocalDate cur = LocalDate.now();
+    MissionAttendance dto = economyRepository.getLastAccessDate(memberId);
+
+    if (!dto.lastAccessDate().equals(cur)) {
+      long updatedRow = economyRepository.setLastAccessDate(cur, memberId);
+    }
+  }
+}
